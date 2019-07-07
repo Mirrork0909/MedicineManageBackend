945df58e3ce94cd9cd04bdf872dd72af3d5698c8
LkFiveeeeeeee <290115116@qq.com>
2019-07-07 13:33:24 +0800

    init version

---
+++ MedicineManageProject/DB/DBContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace MedicineManageProject.DB
+{
+    public class DBContext
+    {
+        public SqlSugarClient Db;
+        public DBContext()
+        {
+            Db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "Data Source=47.102.158.185/orcl;User ID = C##YIYAODB; Password = YIyao123;",
+                DbType = DbType.Oracle,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute
+            });
+
+            Db.Aop.OnLogExecuting = (sql, pars) =>
+            {
+                Console.WriteLine(sql + "\r\n" + Db.Utilities.SerializeObject
+                    (pars.ToDictionary(it => it.ParameterName, it => it.Value)));
+                Console.WriteLine();
+            };
+        }
+
+       
+    }
+}
