5be7b1006ab93beee4e348a5b8444f13954ee0af
LkFiveeeeeeee <290115116@qq.com>
2019-07-08 10:22:55 +0800

    add const message for response

---
+++ MedicineManageProject/Controllers/DiscountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MedicineManageProject.DB.Services;
+using MedicineManageProject.DTO;
+using MedicineManageProject.Utils;
+
+namespace MedicineManageProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        [HttpGet("all")]
+        public IActionResult getDiscountAll()
+        {
+            DiscountManager discountManager = new DiscountManager();
+            List<DiscountDTO> discountDTOs = discountManager.getDiscountDTOs();
+            return Ok(new JsonCreate() { message = Utils.ConstMessage.GET_SUCCESS, data = discountDTOs });
+        }
+
+        [HttpGet("{medicineId}")]
+        public IActionResult getDiscountById(String medicineId)
+        {
+            DiscountManager discountManager = new DiscountManager();
+            List<DiscountDTO> discountDTOs = discountManager.getDiscountDTOById(medicineId);
+            return Ok(new JsonCreate() { message = Utils.ConstMessage.GET_SUCCESS, data = discountDTOs });
+        }
+        
+    }
+
+   
+
+
+}
---
+++ MedicineManageProject/Utils/ConstMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicineManageProject.Utils
+{
+    public static class ConstMessage
+    {
+        public static String GET_SUCCESS = "获取信息成功";
+        public static String NOT_FOUND = "未找到相关信息";
+        public static String CONFILICT = "插入信息失败";
+        public static String UPDATE_SUCCESS = "更新信息成功";
+        public static String INSERT_SUCCESS = "插入信息成功";
+        public static String BAD_REQUEST = "糟糕的请求参数";
+    }
+}
