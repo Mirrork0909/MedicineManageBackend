51791450acafd9f97150d9f073e3b3e7eeb4f686
LkFiveeeeeeee <290115116@qq.com>
2019-07-11 01:18:37 +0800

    qiniu fix

--- MedicineManageProject/Controllers/QiniuController.cs
+++ MedicineManageProject/Controllers/QiniuController.cs
@@ -5,8 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MedicineManageProject.Utils;
-using Qiniu.Util;
-using Qiniu.IO.Model;
+
 
 namespace MedicineManageProject.Controllers
 {
@@ -17,11 +16,8 @@ namespace MedicineManageProject.Controllers
         [HttpGet("token")]
         public IActionResult getToken()
         {
-            Mac mac = new Mac(QiniuMessage.AK, QiniuMessage.SK);
-            Auth auth = new Auth(mac);
-            PutPolicy putPolicy = new PutPolicy();
-            putPolicy.Scope = QiniuMessage.BUCKET;
-            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            QiniuManager qiniuManager = new QiniuManager();
+            var token = qiniuManager.getQiniuToken();
             return Ok(JsonCreate.newInstance(ConstMessage.GET_SUCCESS, token));
         }
     }
---
+++ MedicineManageProject/Utils/QiniuManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedicineManageProject.Utils;
+using Qiniu.Util;
+using Qiniu.IO.Model;
+
+namespace MedicineManageProject.Utils
+{
+    public class QiniuManager
+    {
+        public String getQiniuToken()
+        {
+            Mac mac = new Mac(QiniuMessage.AK, QiniuMessage.SK);
+            Auth auth = new Auth(mac);
+            PutPolicy putPolicy = new PutPolicy();
+            putPolicy.Scope = QiniuMessage.BUCKET;
+            putPolicy.SetExpires(7200 * 12);
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            return token;
+        }
+    }
+}
