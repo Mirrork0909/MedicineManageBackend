f9031d9579a7c672a17dd1a24edb9454b1d70028
LkFiveeeeeeee <290115116@qq.com>
2019-07-10 23:53:43 +0800

    qiniu api

---
+++ MedicineManageProject/Controllers/QiniuController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MedicineManageProject.Utils;
+using Qiniu.Util;
+using Qiniu.IO.Model;
+
+namespace MedicineManageProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QiniuController : ControllerBase
+    {
+        [HttpGet("token")]
+        public IActionResult getToken()
+        {
+            Mac mac = new Mac(QiniuMessage.AK, QiniuMessage.SK);
+            Auth auth = new Auth(mac);
+            PutPolicy putPolicy = new PutPolicy();
+            putPolicy.Scope = QiniuMessage.BUCKET;
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            return Ok(JsonCreate.newInstance(ConstMessage.GET_SUCCESS, token));
+        }
+    }
+}
---
+++ MedicineManageProject/Utils/QiniuMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicineManageProject.Utils
+{
+    public class QiniuMessage
+    {
+        public static String AK = "X6hTCi-g-QkGxc3uE9HHN7g-2Vy64TnU8YlQ020q";
+        public static String SK = "weMcbdosr1Pe6OzqeN-XL5vy6Nj79yvCyPO1Qoco";
+        public static String BUCKET = "medicine_image";
+    }
+}
