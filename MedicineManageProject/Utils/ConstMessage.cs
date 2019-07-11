b8b9096c294038db167369b05e8189153c3291c2
LkFiveeeeeeee <290115116@qq.com>
2019-07-11 17:13:09 +0800

    delete api about discount

--- MedicineManageProject/Utils/ConstMessage.cs
+++ MedicineManageProject/Utils/ConstMessage.cs
@@ -10,10 +10,12 @@ namespace MedicineManageProject.Utils
         public static String GET_SUCCESS = "获取信息成功";
         public static String NOT_FOUND = "未找到相关信息";
         public static String CONFILICT = "插入信息失败";
+        public static String DELETE_FAIL = "删除信息失败";
         public static String UPDATE_SUCCESS = "更新信息成功";
         public static String INSERT_SUCCESS = "插入信息成功";
         public static String BAD_REQUEST = "糟糕的请求参数";
         public static String UPDATE_FAIL = "更新信息失败";
         public static String AUTHORIZATION_EXPIRED = "用户授权过期";
+        public static String DELETE_SUCCESS = "删除成功";
     }
 }
